78f3d5e82941917d65c494080fe44a296b41e702
Stephane Peter <megastep@megastep.org>
2013-10-21 15:48:34 -0700

    Initial Commit

---
+++ bin2c/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  bin2c
+//
+//  Created by Stéphane Peter on 10/21/13.
+//  Copyright (c) 2013 Catloaf Software, LLC. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
