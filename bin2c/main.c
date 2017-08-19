33991fbbbd910f13fb296728360f707a52ff3f5c
King_DuckZ <king_duckz@gmx.com>
2017-08-19 12:34:11 +0100

    Make the 'static' keyword optional in the output.

--- bin2c/main.c
+++ bin2c/main.c
@@ -17,39 +17,49 @@ typedef enum
     VTNSString
 } VariableType;
 
+typedef struct
+{
+    VariableType vtype;
+    int static_keyword;
+    int line_len;
+} OutOptions;
+
 static void print_usage(const char *argv0)
 {
     fprintf(stderr,
-            "Usage: %s [-i inputfile] [-o output.h] [-l line_len] [-t type] -a var_name\n"
+            "Usage: %s [-i inputfile] [-o output.h] [-l line_len] [-t type] [-sh] -a var_name\n"
+            "\t-s make the generated variable static\n"
+            "\t-h show this help\n"
             "\ttype can be: char (unsigned char array, default), nsstring (Objective-C NSString constant)\n",
             argv0);
     exit(1);
 }
 
-void bin2c(const char *infile, const char *outfile, const char *array, int line_len, VariableType vtype)
+void bin2c(const char *infile, const char *outfile, const char *array, const OutOptions* opts)
 {
     FILE *in, *out;
-    
+    const char* const static_str = (opts->static_keyword ? "static " : "");
+
     in = infile ? fopen(infile, "rb") : stdin;
     if (in) {
         out = outfile ? fopen(outfile, "w") : stdout;
         if (out) {
             unsigned char c;
             unsigned int l = 0;
-            
+
             if (infile)
                 fprintf(out, "// Imported from file '%s'\n", infile);
-            if (vtype == VTChar) {
-                fprintf(out, "static const unsigned char %s[] = \n\t\"", array);
+            if (opts->vtype == VTChar) {
+                fprintf(out, "%sconst unsigned char %s[] = \n\t\"", static_str, array);
             } else { // NSString
-                fprintf(out, "static const NSString *%s = \n\t@\"", array);
+                fprintf(out, "%sconst NSString *%s = \n\t@\"", static_str, array);
             }
-            
+
             while (!feof(in)) {
                 c = fgetc(in);
                 l++;
                 fprintf(out, "\\x%02x", c);
-                if ((l % line_len) == 0) {
+                if ((l % opts->line_len) == 0) {
                     fprintf(out, "\"\n\t\"");
                 }
             }
@@ -70,10 +80,14 @@ int main(int argc,  char * const argv[])
 {
     const char *infile = NULL, *outfile = NULL, *array = NULL;
     int line_len = 80;
-    VariableType vtype = VTChar;
+    OutOptions opts;
     int opt;
-    
-    while( (opt = getopt(argc, argv, "i:o:a:l:t:")) != -1) {
+
+    opts.vtype = VTChar;
+    opts.static_keyword = 0;
+    opts.line_len = 80;
+
+    while( (opt = getopt(argc, argv, "i:o:a:l:t:sh")) != -1) {
         switch (opt) {
             case 'i':
                 infile = strdup(optarg);
@@ -85,29 +99,32 @@ int main(int argc,  char * const argv[])
                 array = strdup(optarg);
                 break;
             case 'l':
-                line_len = atoi(optarg);
+                opts.line_len = atoi(optarg);
                 break;
             case 't':
                 if (!strcmp(optarg, "char")) {
-                    vtype = VTChar;
+                    opts.vtype = VTChar;
                 } else if (!strcmp(optarg, "nsstring")) {
-                    vtype = VTNSString;
+                    opts.vtype = VTNSString;
                 } else {
                     print_usage(argv[0]);
                 }
                 break;
+            case 's':
+                opts.static_keyword = 1;
+                break;
+            case 'h':
             default:
                 print_usage(argv[0]);
                 break;
         }
     }
-    
+
     if (!array) {
         print_usage(argv[0]);
     } else {
-        bin2c(infile, outfile, array, line_len, vtype);
+        bin2c(infile, outfile, array, &opts);
     }
-    
+
     return 0;
 }
-
